2edbff504138d4b71ff8e6cb50ab976fe75096ff
Aditya Srinivas <44541502+BASadityaSrinivas@users.noreply.github.com>
2024-07-25 16:46:29 +0530

    [125] [Time Beats: 96.37%] [Memory Beats: 95.68%] - LeetPush

--- 125-Valid-Palindrome.py
+++ 125-Valid-Palindrome.py
@@ -1,9 +1,22 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        new = \\
 
-        for i in s:
-            if i.isalnum():
-                new += i.lower()
-        
-        return new == new[::-1]
+        ## Two pointer approach
+
+        lp = 0
+        rp = len(s)-1
+
+        while rp > lp:
+            if s[lp].isalnum():
+                if s[rp].isalnum():
+                    if s[lp].lower() == s[rp].lower():
+                        rp -= 1
+                        lp += 1
+                    else:
+                        return False
+                else:
+                    rp -= 1
+            else:
+                lp += 1
+
+        return True
