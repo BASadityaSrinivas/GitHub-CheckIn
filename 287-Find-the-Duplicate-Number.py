6501b3b8d054341ac3793f4a05f54e1f5ba955e8
Aditya Srinivas <44541502+BASadityaSrinivas@users.noreply.github.com>
2024-08-14 22:18:13 +0530

    [287] [Time Beats: 96.15%] [Memory Beats: 19.62%] - LeetPush

--- 287-Find-the-Duplicate-Number.py
+++ 287-Find-the-Duplicate-Number.py
@@ -1,9 +1,16 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        arr = [False] * (len(nums)+1)
+        # arr = [False] * (len(nums)+1)
+
+        # for i in nums:
+        #     if not arr[i]:
+        #         arr[i] = True
+        #     else:
+        #         return i
+
+        # sett = set()
 
         for i in nums:
-            if not arr[i]:
-                arr[i] = True
-            else: 
+            if i in sett:
                 return i
+            sett.add(i)
