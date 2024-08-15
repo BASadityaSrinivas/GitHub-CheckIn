696785e49a09d0a963bf3fbac555293d3e949ac6
Aditya Srinivas <44541502+BASadityaSrinivas@users.noreply.github.com>
2024-08-15 15:55:05 +0530

    [509] [Time Beats: 80.08%] [Memory Beats: 54.47%] - LeetPush

--- 509-Fibonacci-Number.py
+++ 509-Fibonacci-Number.py
@@ -1,7 +1,19 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-        return self.fib(n-1) + self.fib(n-2)
+        if n == 0 or n == 1:
+            return n
+
+        arr = [-1] * (n+1)
+        arr[0] = 0
+        arr[1] = 1
+
+        def recur(arr, n):
+            if arr[n] != -1:
+                return arr[n]   
+            else:
+                calc = recur(arr, n-1) + recur(arr, n-2)
+                arr[n] = calc
+            return calc
+        
+        recur(arr, n)
+        return arr[n]
