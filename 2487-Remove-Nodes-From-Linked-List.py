7b1c0bcc8cdb9fda66151de576cb0d2f0de44a17
Aditya Srinivas <44541502+BASadityaSrinivas@users.noreply.github.com>
2024-08-17 15:33:33 +0530

    [2487] [Time Beats: 38.05%] [Memory Beats: 5.11%] - LeetPush

--- 2487-Remove-Nodes-From-Linked-List.py
+++ 2487-Remove-Nodes-From-Linked-List.py
@@ -5,16 +5,33 @@
 #         self.next = next
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        stack = []
-        while head:
-            while stack and stack[-1].val < head.val:
-                stack.pop()
+        # # Solution 1 - Using recursion
+        def recur(prev, cur, maxx):
+            if not cur.next:
+                return cur.val
+            maxx = max(cur.val, recur(cur, cur.next, maxx))
+            if cur.val < maxx:
+                prev.next = cur.next
+            return maxx
 
-            stack.append(head)
-            head = head.next
-            
-        for node in range(len(stack)-1):
-            stack[node].next = stack[node+1]
+        dummy = ListNode(0, head)
+        recur(dummy, dummy.next, 0)
+
+        return dummy.next
         
-        return stack[0]
+        # # Solution 2 - Using Stack
+        # stack = []
+        # while head:
+        #     while stack and stack[-1].val < head.val:
+        #         stack.pop()
+
+        #     stack.append(head)
+        #     head = head.next
+            
+        # for node in range(len(stack)-1):
+        #     stack[node].next = stack[node+1]
+        
+        # return stack[0]
+
+
 
