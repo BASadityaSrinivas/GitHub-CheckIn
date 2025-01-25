21ecd63df0d305530bca604e566a3184a5e10d17
Aditya Srinivas <44541502+BASadityaSrinivas@users.noreply.github.com>
2025-01-25 18:21:12 +0530

    [704] [Time Beats: 100.00%] [Memory Beats: 19.38%] - LeetPush

--- 704-Binary-Search.py
+++ 704-Binary-Search.py
@@ -1,16 +1,15 @@
 class Solution:
-    def binary_search(self, arr, l, r, target):
-        m = (l+r)//2
-        if arr[m] == target:
-            return m
-        elif l >= r:
-            return -1
-        elif arr[m] < target:
-            return self.binary_search(arr, m+1, r, target)
-        else:
-            return self.binary_search(arr, l, m-1, target)
-
     def search(self, nums: List[int], target: int) -> int:
-        return self.binary_search(nums, 0, len(nums)-1, target)
+        l, h = 0, len(nums)-1
 
+        while l <= h:
+            m = (l+h)//2
+
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                h = m - 1
         
+        return -1
